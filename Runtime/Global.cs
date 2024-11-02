426bf8ea14dd8897522cd50a35969de68381705e
pandravrc <162582491+pandravrc@users.noreply.github.com>
2024-11-02 10:59:57 +0900

    Update Global.cs

--- Runtime/Global.cs
+++ Runtime/Global.cs
@@ -2,10 +2,8 @@
 
 namespace com.github.pandrabox.pandravase.runtime
 {
-    [AddComponentMenu("Pan/Pan Merge BlendTree")]
     public static class Global
     {
-        public static bool PDEBUGMODE = false;
-        public const float FPS = 60;
+        //Utilに全て移動した。後方互換性のためにに残しているが不要。
     }
 }
