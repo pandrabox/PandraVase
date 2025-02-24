4b33b8952742942710b76d33fce4a1c6c60da464
pandravrc <162582491+pandravrc@users.noreply.github.com>
2025-02-25 07:09:28 +0900

    クラス名の誤りを修正

--- Editor/AnimationClipAnalyzer.cs
+++ Editor/AnimationClipAnalyzer.cs
@@ -15,38 +15,35 @@ namespace com.github.pandrabox.pandravase.editor
     /// ProjectにおけるAnimationClipで実行するとAnimationClipsBuilder相当のコードを生成してコンソール・クリップボードに返す開発ツール
     /// 現状(2025/02/11)において取得するのは名称とBindのみです。つまり、キーは含みません（ループ順序に悩んだため）
     /// </summary>
-    public class AnimationClipAnalyzer
-    {
 
 #if PANDRADBG
-        public class FxSortDebug
+    public class AnimationClipAnalyzer
+    {
+        [MenuItem("Assets/PanDev/AnimationClipAnalyzer")]
+        public static void AnimationClipAnalyzerMain()
         {
-            [MenuItem("Assets/PanDev/AnimationClipAnalyzer")]
-            public static void AnimationClipAnalyzer()
-            {
-                var sb = new StringBuilder();
-                var selectedObject = Selection.activeObject;
-                if (selectedObject == null || (!(selectedObject is AnimationClip))) return;
-                AnimationClip tgt = selectedObject as AnimationClip;
-                sb.AppendLine($@"ab.Clip(""{tgt.name}"")");
-                EditorCurveBinding[] curveBindings = AnimationUtility.GetCurveBindings(tgt);
+            var sb = new StringBuilder();
+            var selectedObject = Selection.activeObject;
+            if (selectedObject == null || (!(selectedObject is AnimationClip))) return;
+            AnimationClip tgt = selectedObject as AnimationClip;
+            sb.AppendLine($@"ab.Clip(""{tgt.name}"")");
+            EditorCurveBinding[] curveBindings = AnimationUtility.GetCurveBindings(tgt);
 
-                foreach (EditorCurveBinding binding in curveBindings)
-                {
-                    var type = binding.type.ToString().Replace("UnityEngine.", "").Replace("Animations.", "");
-                    sb.AppendLine($@"   .Bind(""{binding.path}"", typeof({type}), ""{binding.propertyName}"")");
-                    //AnimationCurve curve = AnimationUtility.GetEditorCurve(tgt, binding);
-                    //for (var i = 0; i < curve.keys.Length; i++)
-                    //{
-                    //    Keyframe key = curve.keys[i];
-                    //    Debug.LogWarning(key.time + ", " + key.value);
-                    //}
-                }
-                string result = sb.ToString();
-                Debug.LogWarning(result);
-                EditorGUIUtility.systemCopyBuffer = result;
+            foreach (EditorCurveBinding binding in curveBindings)
+            {
+                var type = binding.type.ToString().Replace("UnityEngine.", "").Replace("Animations.", "");
+                sb.AppendLine($@"   .Bind(""{binding.path}"", typeof({type}), ""{binding.propertyName}"")");
+                //AnimationCurve curve = AnimationUtility.GetEditorCurve(tgt, binding);
+                //for (var i = 0; i < curve.keys.Length; i++)
+                //{
+                //    Keyframe key = curve.keys[i];
+                //    Debug.LogWarning(key.time + ", " + key.value);
+                //}
             }
+            string result = sb.ToString();
+            Debug.LogWarning(result);
+            EditorGUIUtility.systemCopyBuffer = result;
         }
-#endif
     }
+#endif
 }
