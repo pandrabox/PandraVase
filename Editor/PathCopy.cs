bb5475d76286ee1e13966ad6ba1946d8fd399dc3
pandravrc <162582491+pandravrc@users.noreply.github.com>
2025-02-25 07:08:22 +0900

    PathCopy実装

---
+++ Editor/PathCopy.cs
@@ -0,0 +1,29 @@
+﻿using com.github.pandrabox.pandravase.runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+using static com.github.pandrabox.pandravase.editor.Util;
+
+
+namespace com.github.pandrabox.pandravase.editor
+{
+
+#if PANDRADBG
+    public class PathCopy
+    {
+        [MenuItem("Assets/PanDev/PathCopy")]
+        public static void AnimationClipAnalyzer()
+        {
+            var selectedObject = Selection.activeObject;
+            if (selectedObject == null) return;
+            string result = AssetDatabase.GetAssetPath(selectedObject);
+            Debug.LogWarning(result);
+            EditorGUIUtility.systemCopyBuffer = result;
+        }
+    }
+#endif
+}
